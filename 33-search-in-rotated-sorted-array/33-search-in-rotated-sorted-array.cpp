7305dd5f9c06557f2b2b27a313f970cbabb8dee0
luvgameon <110060269+luvgameon@users.noreply.github.com>
2022-09-14 18:31:20 +0530

    Time: 12 ms (14.97%), Space: 11 MB (74.92%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,40 +1,65 @@
 class Solution {
 public:
-    int search(vector<int>&arr , int target) {
-       
-        int l=0;
-        int r=arr.size()-1;
-        int mid=0;
-        while(l<=r)
+    int getpivot(vector<int>& nums)
+    {
+        int s=0;
+        int e=nums.size()-1;
+        int mid=s+(e-s)/2;
+        while(s<e)
         {
-            mid=l+(r-l)/2;
-            if(arr[mid]==target)
+            if(nums[mid]>=nums[0])
             {
-                return mid;
+                s=mid+1;
+
             }
-            else if(arr[l]<=arr[mid])
+            else{
+                e=mid;
+            }
+            mid=s+(e-s)/2;
+        }
+        return s;
+
+    }
+    int Binary(vector<int>& nums, int s,int e,int target)
+    {
+         
+        int mid=s+(e-s)/2;
+        while(s<=e)
+        {
+            if(nums[mid]==target)
             {
-                if(arr[l]<=target && arr[mid]>=target)
-                    r=mid-1;
-                else
-                {
-                    l=mid+1;
-                }
+               return mid;
             }
-            else
+            else if(nums[mid]>target)
             {
-                if(arr[mid]<=target && arr[r]>=target)
-                {
-                    l=mid+1;
-                }
-                else{
-                    r=mid-1;
-                }
+                e=mid-1;
+
             }
+            else{
+                s=mid+1;
+            }
+            mid=s+(e-s)/2;
+            
         }
         return -1;
-    }
-
         
-    
+    }
+    int search(vector<int>& nums, int target) {
+        int pivot=getpivot(nums);
+        int s=0;
+        int e=nums.size()-1;
+        int mid=s+(e-s)/2;
+        
+         if(target>= nums[pivot] && target <= nums[e])
+        {
+            return Binary(nums,pivot,e,target);
+        }
+        else{
+           
+         return Binary(nums,0,pivot-1,target);
+            
+        }
+        return -1;
+        
+    }
 };
