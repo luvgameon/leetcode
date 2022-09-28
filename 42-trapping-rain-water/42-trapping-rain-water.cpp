42a2114f473908978a7f584d9ea2d40fca330da0
luvgameon <110060269+luvgameon@users.noreply.github.com>
2022-09-28 14:01:18 +0530

    Time: 27 ms (60.59%), Space: 20.3 MB (42.47%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n=height.size();
-       int left[n];
-        int right[n];
-        left[0]=height[0];
-        right[n-1]=height[n-1];
-        int ans=0;
-        for(int i=1;i<n;i++)
+    int trap(vector<int>& arr) {
+         stack<int>s;
+        long long int ans=0;
+        int n=arr.size();
+        for(int i=0;i<n;i++)
         {
-             left[i]=max(left[i-1],height[i]);
-        
-        }
-         for(int i=n-2;i>=0;i--)
-        {
-             right[i]=max(right[i+1],height[i]);
-        }
-        for(int i=0;i<height.size();i++)
-        {
-            int temp=min(left[i],right[i]);
-            temp=temp-height[i];
-            ans+=temp;
-
+            while(!s.empty() && arr[i]>arr[s.top()])
+            {
+                
+              long long int top=s.top();
+               s.pop();
+               if(s.empty())break;
+               long long int distance=(i-s.top()-1);
+               long long int height=min(arr[i],arr[s.top()])-arr[top];
+               ans+=height*distance;
+                
+                
+            }
+            s.push(i);
+            
         }
         return ans;
-        
-        
     }
+        // code here
 };
