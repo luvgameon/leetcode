b7c0a90412686c5823128e9f4b53a0821aafac1f
luvgameon <110060269+luvgameon@users.noreply.github.com>
2022-11-13 13:58:41 +0530

    Added solution - LeetHub

---
+++ Palindrome String - GFG/palindrome-string.js
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let S = readLine().trim();
+        let obj = new Solution();
+        let res=obj.isPalindrome(S);
+        console.log(res);
+    
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {string} S
+ * @return {number}
+*/
+
+class Solution {
+    
+   
+        isPalindrome(string){
+        let s=0;
+        let e=string.length-1;
+        while(s<e){
+            if(string[s]!=string[e])
+            {
+                return 0;
+            }
+            s++;
+            e--;
+        }
+        return 1;
+        }
+        
+}
