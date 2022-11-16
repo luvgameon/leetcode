c9cab39aa00795572d91f251a7e0a421a1eaa110
luvgameon <110060269+luvgameon@users.noreply.github.com>
2022-11-16 13:16:09 +0530

    Added solution - LeetHub

---
+++ Sum of Beauty of All Substrings - GFG/sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int beautySum(string s) {
+      
+      int n=s.size();
+
+         int sum=0;
+
+            for(int i=0;i<n;i++)
+
+        {
+
+               int freq[26]={0};
+
+           for(int j=i;j<n;j++){
+
+                int mi=INT_MAX,ma=INT_MIN;
+
+                freq[s[j]-'a']++;
+
+                for(int k=0;k<26;k++){
+
+                     if(freq[k]!=0)
+
+                     {
+
+                        mi = min(mi,freq[k]);
+
+                         ma = max(ma,freq[k]);
+
+                     }
+
+                }
+
+                 sum += (ma-mi);
+
+            }
+
+        }
+
+         return sum;
+
+ 
+
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution obj;
+        cout << obj.beautySum(s) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
