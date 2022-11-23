d29b0304bb51870ef3ae578b3da41f4f31115fac
luvgameon <110060269+luvgameon@users.noreply.github.com>
2022-11-23 17:04:59 +0530

    Time: 19 ms (42.29%), Space: 54.5 MB (11.80%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,69 +1,20 @@
 class Solution {
-public:
-    string reverseWords(string s) {
-        string ans="";
-        string temp="";
-        int n=s.size();
-        for(int i=0;i<n;i++)
-        {
-            if(s[i]!=' ')
-            {
-                temp+=s[i];
-            }
-            else if(temp!="")
-            {
-                if(ans==""){
-                    ans=temp;
-                }
-                else{
-                    ans=temp+" "+ans;
-                    
-                }
-                temp.clear();
-                
-            }
-        } 
-            
-         if(temp != "") {
-            if(ans != "") ans = temp + " " + ans;
-            else ans = temp;
-        }
+    public String reverseWords(String s) {
+        int i=s.length()-1;
+     String ans="";
+     while(i>=0){
+         
+         
+         while(i>=0 && s.charAt(i)==' ') i--;
+         int j=i;
+         while(i>=0 && s.charAt(i)!=' ') i--;
+         if(ans.isEmpty()){
+            ans=ans.concat(s.substring(i+1,j+1));
+         }else{
+             ans=ans.concat(" "+s.substring(i+1,j+1));
+         }
+     }
+     return ans.trim();
         
-           
-
-        
-        return ans;
-                
     }
-    
-        /* int i = 0, N = s.length();
-        string buffer = "", res = "";
-
-        for(int i = 0; i < N; i++) {
-            char ch = s[i];
-
-            if(ch != ' ') buffer += ch;
-            else if(buffer != "") { // only if there's something in buffer
-                if(res == "") res = buffer;
-                else res = buffer + " " + res; // add word before prev with a space
-                buffer.clear();
-            }
-        }
-        if(buffer != "") {
-            if(res != "") res = buffer + " " + res;
-            else res = buffer;
-        }
-
-        return res;
-    }*/
-    
-};
-            
-            
-            
-
-        
-        
-       
-        
-        
+}
